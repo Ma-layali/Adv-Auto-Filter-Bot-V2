52d1242b8d700493eb682414f821fe04bc1df16b
Ma-layali <83540012+Ma-layali@users.noreply.github.com>
2021-09-24 11:49:10 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = "<code>" + file_name + "</code>" "@MzoneChat",
+                caption = "<code>" + file_name + "</code>" "\n\n ♾ @MzoneChat ♾",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
